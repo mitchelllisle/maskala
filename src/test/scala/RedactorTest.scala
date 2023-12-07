fa3b0302f72c70262eef3cbb48fb3abf297106ce
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-12-07 22:41:55 +1100

    more tests

--- src/test/scala/RedactorTest.scala
+++ src/test/scala/RedactorTest.scala
@@ -1,14 +1,22 @@
-import org.mitchelllisle.redaction.{HashingStrategy, Redactor}
+import org.mitchelllisle.redaction.{HashingStrategy, MaskingStrategy, Redactor}
 
 class RedactorTest extends SparkFunSuite {
 
   "Giving multiple strategies" should "apply the right transformations" in {
+    val mask = "{REDACTED}"
     val strategies = Seq(
-      HashingStrategy("user_id")
+      HashingStrategy("user_id"),
+      MaskingStrategy("movie", mask)
     )
     val redactor = new Redactor(strategies)
     val redacted = redactor(sampleNetflixData)
 
-    assert(redacted.collect().forall(_.getString(0).length == 64))
+    redacted.collect().foreach(row => {
+      val hashed = row.getString(0)
+      val masked = row.getString(3)
+
+      assert(hashed.length == 64)
+      assert(masked == mask)
+    })
   }
 }
