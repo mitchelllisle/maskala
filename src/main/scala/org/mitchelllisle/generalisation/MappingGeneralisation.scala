74879c567b7b82928bbd5b80318adba7e3474035
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-12-10 19:59:31 +1100

    documentation

--- src/main/scala/org/mitchelllisle/generalisation/MappingGeneralisation.scala
+++ src/main/scala/org/mitchelllisle/generalisation/MappingGeneralisation.scala
@@ -2,6 +2,13 @@ package org.mitchelllisle.generalisation
 
 import org.apache.spark.sql.{DataFrame, functions => F}
 
+/** A generalisation strategy that applies a mapping to a specific column in a DataFrame.
+  *
+  * @param columnName
+  *   The name of the column to apply the mapping to.
+  * @param mapping
+  *   A map of input values to their corresponding output values.
+  */
 case class MappingGeneralisation(columnName: String, mapping: Map[String, String]) extends GeneralisationStrategy {
 
   private val mappingUdf = F.udf((input: String) => mapping.getOrElse(input, input))
