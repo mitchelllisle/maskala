f27103431d4956d10a536898c7936bdd9042a0cc
Mitchell Lisle <lislemitchell@gmail.com>
2023-12-27 17:13:39 +1100

    add more docs

--- src/main/scala/org/mitchelllisle/anonymisers/AnonymiserStrategy.scala
+++ src/main/scala/org/mitchelllisle/anonymisers/AnonymiserStrategy.scala
@@ -2,8 +2,31 @@ package org.mitchelllisle.anonymisers
 
 import org.apache.spark.sql.DataFrame
 
+/** A trait defining the strategy for data anonymisation.
+ *
+ * This trait acts as a contract for implementing different anonymisation strategies.
+ * Classes implementing this trait should provide the logic for how data anonymisation is to be applied on a DataFrame.
+ *
+ * The primary method `apply` should be implemented to take a DataFrame and anonymisation parameters,
+ * and return a DataFrame where the anonymisation strategy has been applied.
+ */
 trait AnonymiserStrategy {
+
+  /** Applies the anonymisation strategy to the provided DataFrame.
+   *
+   * This method should be implemented by concrete classes to specify how the anonymisation is to be performed.
+   * It takes a DataFrame and anonymisation parameters, and returns a DataFrame with the anonymisation applied.
+   *
+   * @param data The DataFrame to be anonymised.
+   * @param config The configuration parameters for anonymisation, encapsulated in an AnonymisationParams instance.
+   * @return The anonymised DataFrame.
+   */
   def apply(data: DataFrame, config: AnonymisationParams): DataFrame
 }
 
+/** A trait for encapsulating configuration parameters for anonymisation strategies.
+ *
+ * This trait serves as a base for defining various types of configuration parameters needed by different anonymisation strategies.
+ * Concrete implementations of this trait will hold specific configuration details required by respective anonymisation strategies.
+ */
 trait AnonymisationParams
--- src/main/scala/org/mitchelllisle/anonymisers/MappingStrategy.scala
+++ src/main/scala/org/mitchelllisle/anonymisers/MappingStrategy.scala
@@ -2,25 +2,58 @@ package org.mitchelllisle.anonymisers
 
 import org.apache.spark.sql.{DataFrame, functions => F}
 
+/** Represents parameters for mapping-based anonymisation.
+ *
+ * @param mapping A string representing a set of key-value pairs for mapping.
+ *                Each pair is separated by a comma, and the key and value within a pair are separated by an equals sign.
+ *                For example, "original1=anonymised1,original2=anonymised2".
+ */
 case class MappingParams(mapping: String) extends AnonymisationParams
 
-/** A generalisation strategy that applies a mapping to a specific column in a DataFrame.
-  *
-  * @param columnName
-  *   The name of the column to apply the mapping to.
-  */
+/** Implements a generalisation strategy for data anonymisation by applying a mapping to a specific column in a DataFrame.
+ *
+ * This strategy takes a column name and replaces its values based on a provided mapping. The mapping is defined in `MappingParams`.
+ * Values in the specified column that are found in the mapping keys are replaced with the corresponding mapping values.
+ * Values not found in the mapping are left unchanged.
+ *
+ * @param columnName The name of the column in the DataFrame to which the mapping will be applied.
+ */
 case class MappingStrategy(columnName: String) extends AnonymiserStrategy {
+
+  /** Converts a mapping string to a Map.
+   *
+   * The method splits the input string by commas to get key-value pairs,
+   * then further splits each pair by the equals sign to separate keys and values.
+   * It finally collects these into a Map.
+   *
+   * @param mapping The mapping string to convert.
+   * @return A Map representing the key-value pairs from the input string.
+   */
   private def mappingStringToMap(mapping: String): Map[String, String] = {
     val keyValuePairs = mapping.split(",").map(_.trim.split("="))
     keyValuePairs.collect { case Array(key, value) => key -> value}.toMap
   }
 
+  /** Applies the mapping strategy to a DataFrame.
+   *
+   * This method applies the mapping to the specified column in the DataFrame.
+   * It uses a UDF to map each value in the column based on the provided `MappingParams`.
+   * If a value in the column matches a key in the mapping, it is replaced with the corresponding value from the mapping.
+   * Otherwise, the original value is retained.
+   *
+   * @param data The DataFrame to which the mapping will be applied.
+   * @param params The parameters defining the mapping. Must be an instance of `MappingParams`.
+   * @return A DataFrame with the mapping applied to the specified column.
+   * @throws IllegalArgumentException if `params` is not an instance of `MappingParams`.
+   */
   override def apply(data: DataFrame, params: AnonymisationParams): DataFrame = {
     params match {
       case mp: MappingParams =>
         val mappingValues = mappingStringToMap(mp.mapping)
         val mappingUdf = F.udf((input: String) => mappingValues.getOrElse(input, input))
         data.withColumn(columnName, mappingUdf(F.col(columnName)))
+      case _ =>
+        throw new IllegalArgumentException("MappingStrategy requires MappingParams")
     }
   }
 }
