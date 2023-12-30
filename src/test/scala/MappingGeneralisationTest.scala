d1706b65ceb450d811cd60b5e1274a09c07a89e9
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-12-30 23:17:04 +1100

    fix mapping tests

--- src/test/scala/MappingGeneralisationTest.scala
+++ src/test/scala/MappingGeneralisationTest.scala
@@ -1,19 +1,19 @@
-//import org.mitchelllisle.anonymisers.MappingStrategy
-//import org.scalatest.flatspec.AnyFlatSpec
-//
-//
-//class MappingGeneralisationTest extends AnyFlatSpec with SparkFunSuite {
-//  import spark.implicits._
-//
-//  "MappingGeneralisation" should "correctly map values based on provided mapping" in {
-//    val data = Seq("A", "B", "C", "D", "E").toDF("Letters")
-//
-//    val mapping = Map("A" -> "X", "B" -> "Y", "C" -> "Z")
-//    val strategy = MappingStrategy("Letters", mapping)
-//
-//    val generalizedData = strategy(data)
-//    val results = generalizedData.collect().map(row => row.getString(0))
-//
-//    assert(results sameElements Array("X", "Y", "Z", "D", "E"))
-//  }
-//}
+import org.mitchelllisle.anonymisers.{MappingParams, MappingStrategy}
+import org.scalatest.flatspec.AnyFlatSpec
+
+
+class MappingGeneralisationTest extends AnyFlatSpec with SparkFunSuite {
+  import spark.implicits._
+
+  "MappingGeneralisation" should "correctly map values based on provided mapping" in {
+    val data = Seq("A", "B", "C", "D", "E").toDF("Letters")
+
+    val mapping = "A=X,B=Y,C=Z"
+    val strategy = MappingStrategy("Letters")
+
+    val generalizedData = strategy(data, MappingParams(mapping))
+    val results = generalizedData.collect().map(row => row.getString(0))
+
+    assert(results sameElements Array("X", "Y", "Z", "D", "E"))
+  }
+}
